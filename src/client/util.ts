235fe7f91ff7b7abde664ebad05791126805e3ca
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2024-06-22 17:21:24 +0100

    Initial commit

---
+++ src/client/util.ts
@@ -0,0 +1,12 @@
+import {
+  Keypair,
+} from '@solana/web3.js';
+import fs from 'mz/fs';
+
+export async function createKeypairFromFile(
+  filePath: string,
+): Promise<Keypair> {
+  const secretKeyString = await fs.readFile(filePath, 'utf8');
+  const secretKey = Uint8Array.from(JSON.parse(secretKeyString));
+  return Keypair.fromSecretKey(secretKey);
+}
